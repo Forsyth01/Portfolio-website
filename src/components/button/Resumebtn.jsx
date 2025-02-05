43d2feefb9f77204922c53a85aff0e932cd2fc17
FORSYTH OKOEGUALE <forsythokoeguale01@gmail.com>
2025-02-05 09:44:22 +0100

    CV update

--- src/components/button/Resumebtn.jsx
+++ src/components/button/Resumebtn.jsx
@@ -4,7 +4,7 @@ import { FiDownload, FiFile } from "react-icons/fi";
 const Resumebtn = () => {
   return (
     <div>
-      <a href="/FORSYTHCV.pdf" download="FORSYTHCV.pdf">
+      <a href="/CV.pdf" download="CV.pdf">
         <button className="flex gap-2 items-center hover:scale-[106%]  sm:m-0 transition-all bg-white text-[#1d1d1d] sm:px-8 py-2 px-4 rounded-md sm:rounded-full duration-500 text-center">
           <FiFile className=" text-black" />
           <p className=" text-[#1d1d1d]  rounded-full text-center text-sm">
