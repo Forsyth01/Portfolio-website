35c98f35767e5cec3b9fe121f214a07a7847e0af
FORSYTH OKOEGUALE <forsythokoeguale01@gmail.com>
2025-11-12 01:15:44 +0100

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ import Footer from "@/components/Footer";
 const Home = () => {
   return (
     <div className="">
-      {/* <Hero/> */}
+      <Hero/>
       {/* <SectionDivider /> */}
       <SkillsSwiper />
       {/* <SectionDivider /> */}
