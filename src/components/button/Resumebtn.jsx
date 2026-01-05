0bba2f32a6a4535eb41495d8af0371d0283d0639
forsyth01 <forsythokoeguale01@gmail.com>
2026-01-05 17:48:23 +0100

    updatedcv

--- src/components/button/Resumebtn.jsx
+++ src/components/button/Resumebtn.jsx
@@ -8,8 +8,8 @@ const Resumebtn = () => {
         <button className="flex gap-2 items-center hover:scale-[106%]  sm:m-0 transition-all bg-white text-[#1d1d1d] sm:px-8 py-2 px-4 rounded-md sm:rounded-full duration-500 text-center">
           <FiFile className=" text-black" />
           <p className=" text-[#1d1d1d]  rounded-full text-center text-sm">
-            Resume  
-          </p>
+            CV
+          </p> 
         </button>
       </a>
     </div>
